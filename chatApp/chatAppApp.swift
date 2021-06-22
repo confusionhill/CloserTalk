ed535713740c5d66a8a13555868dafa8a7d74378
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-06-22 19:24:07 +0700

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Farhandika Zahrir Mufti guenia on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
